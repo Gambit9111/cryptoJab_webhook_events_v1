4b733d1a64e3b269e4309b6221fb84a09e0f8acf
Gambit9111 <bitgam911@gmail.com>
2023-11-05 16:18:29 -0500

    stripe webhook fix 3

--- payments.py
+++ payments.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from telebot import types, TeleBot
 import stripe
+import json
 
 from flask import (
     Blueprint, request, jsonify
@@ -30,24 +31,60 @@ def stripe_webhook():
     payload = request.data
 
     try:
-        event = stripe.Event.construct_from(
-            payload, stripe.api_key
-        )
-    except ValueError as e:
-        # Invalid payload
-        raise e
+        event = json.loads(payload)
+    except json.decoder.JSONDecodeError as e:
+        print('⚠️  Webhook error while parsing basic request.' + str(e))
+        return jsonify(success=False)
+
+    if STRIPE_ENDPOINT_SECRET:
+        # Only verify the event if there is an endpoint secret defined
+        # Otherwise use the basic event deserialized with json
+        sig_header = request.headers.get('stripe-signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, STRIPE_ENDPOINT_SECRET
+            )
+        except stripe.error.SignatureVerificationError as e:
+            print('⚠️  Webhook signature verification failed.' + str(e))
+            return jsonify(success=False)
     
 
     # Handle the event
-    if event.type == 'invoice.payment_succeeded':
-        invoice_payment = event.data.object
-        print(invoice_payment)
-    
-    elif event.type == 'invoice.payment_failed':
-        invoice_payment = event.data.object
+    if event and event['type'] == 'invoice.payment_succeeded':
+        invoice_payment = event['data']['object']
         print(invoice_payment)
 
-    return jsonify({'message': 'Stripe Webhook'})
+    else:
+        # Unexpected event type
+        print('Unhandled event type {}'.format(event['type']))
+
+    return jsonify(success=True)
+
+
+
+
+
+
+
+    # try:
+    #     event = stripe.Event.construct_from(
+    #         payload, os.getenv('STRIPE_API_KEY')
+    #     )
+    # except ValueError as e:
+    #     # Invalid payload
+    #     raise e
+    
+
+    # # Handle the event
+    # if event.type == 'invoice.payment_succeeded':
+    #     invoice_payment = event.data.object
+    #     print(invoice_payment)
+    
+    # elif event.type == 'invoice.payment_failed':
+    #     invoice_payment = event.data.object
+    #     print(invoice_payment)
+
+    # return jsonify({'message': 'Stripe Webhook'})
 
 
 
