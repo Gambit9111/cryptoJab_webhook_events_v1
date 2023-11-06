63303a2694b561c5514b3d6775fba6f03079fb8d
Gambit9111 <bitgam911@gmail.com>
2023-11-05 23:57:38 -0500

    added coinbase webghook

--- payments.py
+++ payments.py
@@ -1,32 +1,46 @@
 from datetime import datetime, timedelta
-from telebot import types, TeleBot
-import stripe
 import json
+
+from telebot import types, TeleBot
+
+import stripe
+
+from coinbase_commerce.webhook import Webhook
+from coinbase_commerce.error import SignatureVerificationError, WebhookInvalidPayload
 
 from flask import (
     Blueprint, request, jsonify
 )
 
+
+# import scripts
 from db import Database
 
+# Load variables
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Stripe
+# *Stripe
 stripe.api_key = os.getenv('STRIPE_API_KEY')
 STRIPE_ENDPOINT_SECRET = os.getenv('STRIPE_ENDPOINT_SECRET')
 
-# Telegram
+# *Coinbase
+COINBASE_ENDPOINT_SECRET = os.getenv('COINBASE_ENDPOINT_SECRET')
+
+# *Telegram
 TELEGRAM_BOT_API_TOKEN = os.getenv("TELEGRAM_BOT_API_KEY")
 bot = TeleBot(TELEGRAM_BOT_API_TOKEN, num_threads=4, parse_mode="HTML")
 
-# register the payments blueprint
+# register the payments blueprint with flask app
 bp = Blueprint('payments', __name__, url_prefix='/payments')
 
+
+# ! stripe webhook
 @bp.route('/stripe/webhook', methods=['POST'])
 def stripe_webhook():
 
+    # ? stripe stuff
     event = None
     payload = request.data
 
@@ -49,7 +63,7 @@ def stripe_webhook():
             return jsonify(success=False)
     
 
-    # Handle the event
+    # ? Handle our events
     if event and event['type'] == 'invoice.payment_succeeded':
         invoice_payment = event['data']['object']
         print(invoice_payment)
@@ -63,67 +77,29 @@ def stripe_webhook():
 
 
 
-
-
-
-    # try:
-    #     event = stripe.Event.construct_from(
-    #         payload, os.getenv('STRIPE_API_KEY')
-    #     )
-    # except ValueError as e:
-    #     # Invalid payload
-    #     raise e
-    
-
-    # # Handle the event
-    # if event.type == 'invoice.payment_succeeded':
-    #     invoice_payment = event.data.object
-    #     print(invoice_payment)
-    
-    # elif event.type == 'invoice.payment_failed':
-    #     invoice_payment = event.data.object
-    #     print(invoice_payment)
-
-    # return jsonify({'message': 'Stripe Webhook'})
-
-
-
-
-    # # ? Stripe stuff to get the event
-    # event = None
-    # payload = request.data
-    # sig_header = request.headers('STRIPE_SIGNATURE')
-    # try:
-    #     event = stripe.Webhook.construct_event(
-    #         payload, sig_header, STRIPE_ENDPOINT_SECRET
-    #     )
-    # except ValueError as e:
-    #     # Invalid payload
-    #     raise e
-    # except stripe.error.SignatureVerificationError as e:
-    #     # Invalid signature
-    #     raise e
-    
-    # # ! Handle 2 types of events, 'invoice.payment_succeeded' and 'invoice.payment_failed'
-
-    # match event['type']:
-    #     case 'invoice.payment_succeeded':
-    #         invoice_payment = event['data']['object']
-    #         print(invoice_payment)
-
-
-
-
-    # return jsonify({'message': 'Stripe Webhook'})
-
-@bp.route('/coinbase/webhook', methods=['GET'])
+# ! coinbase webhook
+@bp.route('/coinbase/webhook', methods=['POST'])
 def coinbase_webhook():
-    db = Database()
-    print(db)
 
+    # coinbase stuff
+    request_data = request.data.decode('utf-8')
+    # webhook signature verification
+    request_sig = request.headers.get('X-CC-Webhook-Signature', None)
 
+    try:
+        # signature verification and event object construction
+        event = Webhook.construct_event(request_data, request_sig, COINBASE_ENDPOINT_SECRET)
+    except (WebhookInvalidPayload, SignatureVerificationError) as e:
+        return str(e), 400
 
+    print("Received event: id={id}, type={type}".format(id=event.id, type=event.type))
+    # print(event["data"]["description"])
 
+    # # event once the payment is confirmed
 
-    db.close()
-    return jsonify({'message': 'Coinbase Webhook'})
+    if event.type == 'charge:confirmed':
+        print(event["data"])
+        telegram_id = event["data"]["description"]
+        print(telegram_id, "telegram_id just bought a sub")
+
+    return 'success', 200
